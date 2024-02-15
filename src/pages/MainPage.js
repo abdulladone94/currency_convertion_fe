a9a47f520fefa4917a9a5c39130da347f01e777c
abdulladone94 <abdullaf@booleanlabs.biz>
2024-02-15 17:55:32 +0530

    created currency conversion form

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function MainPage() {
+  const [date, setDate] = useState(null);
+  const [sourceCurrency, setSourceCurrency] = useState('');
+  const [targetCurrency, setTargetCurrency] = useState('');
+  const [amountInSourceCurrency, setAmountInSourceCurrency] = useState(0);
+  const [amountIntargetCurrency, setAmountInTargetCurrency] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(date, sourceCurrency, targetCurrency, amountInSourceCurrency);
+  };
+
   return (
     <div>
       <h1 className="lg:mx-32 text-5 font-bold text-green-500">
@@ -13,6 +24,89 @@ export default function MainPage() {
         the value of your money in different currencies, this tool is here to
         help.
       </p>
+
+      <div className="mt-5 flex items-center justify-center">
+        <section className="w-full lg:w-1/2">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label
+                htmlFor={date}
+                className="block mb-2 text-sm font-medium text-white-900 dark:text-white"
+              >
+                Date
+              </label>
+              <input
+                onChange={(e) => setDate(e.target.value)}
+                type="Date"
+                id={date}
+                name={date}
+                className="bg-green-50 border border-green-300 text-green-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                placeholder="name@flowbite.com"
+                required
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor={sourceCurrency}
+                className="block mb-2 text-sm font-medium text-white-900 dark:text-white"
+              >
+                Source Currency
+              </label>
+              <select
+                onChange={(e) => setSourceCurrency(e.target.value)}
+                className="bg-green-50 border border-green-300 text-green-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                name={sourceCurrency}
+                id={sourceCurrency}
+                value={sourceCurrency}
+              >
+                <option value="">Select source currency</option>
+              </select>
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor={targetCurrency}
+                className="block mb-2 text-sm font-medium text-white-900 dark:text-white"
+              >
+                Target Currency
+              </label>
+              <select
+                onChange={(e) => setTargetCurrency(e.target.value)}
+                className="bg-green-50 border border-green-300 text-green-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                name={targetCurrency}
+                id={targetCurrency}
+                value={targetCurrency}
+              >
+                <option value="">Select target currency</option>
+              </select>
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor={amountInSourceCurrency}
+                className="block mb-2 text-sm font-medium text-white-900 dark:text-white"
+              >
+                Amount in source currency
+              </label>
+              <input
+                onChange={(e) => setAmountInSourceCurrency(e.target.value)}
+                type="number"
+                id={amountInSourceCurrency}
+                name={amountInSourceCurrency}
+                value={amountInSourceCurrency}
+                className="bg-green-50 border border-green-300 text-green-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                placeholder="Amount in source currency"
+                required
+              />
+            </div>
+
+            <button className="bg-green-600 hover:bg-green-700 text-white py-2 px-4 font-medium rounded-md">
+              Get the target currency
+            </button>
+          </form>
+        </section>
+      </div>
     </div>
   );
 }
